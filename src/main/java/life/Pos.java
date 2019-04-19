7e79e2ab1490096b2e151b9efee7f825a3947109
Felix Na <felix.na70@gmail.com>
2019-04-19 23:04:32 +0200

    shorten

--- src/main/java/life/Pos.java
+++ src/main/java/life/Pos.java
@@ -17,11 +17,8 @@ public final class Pos {
         Set<Pos> res = HashSet.of(Pos.of(x - 1, y - 1), Pos.of(x - 1, y), Pos.of(x - 1, y + 1),
                 Pos.of(x, y + 1), Pos.of(x + 1, y + 1), Pos.of(x + 1, y),Pos.of(x + 1, y - 1),
                 Pos.of(x, y - 1));
-        return res.map(p -> p.wrap(boardWidth, boardHeight));
+        return res.map(p -> (p.x >= 0 && p.x < boardWidth && p.y >= 0 && p.y < boardHeight) ?
+                p : Pos.of(Math.floorMod(p.x, boardWidth), Math.floorMod(p.y, boardHeight)));
     }
 
-    Pos wrap(int boardWidth, int boardHeight) {
-        return (x >= 0 && x < boardWidth && y >= 0 && y < boardHeight) ?
-                this : Pos.of(Math.floorMod(x, boardWidth), Math.floorMod(y, boardHeight));
-    }
 }
